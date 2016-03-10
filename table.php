ac33e535af98a2a5291815e3f200477acca27683
dmikab <dmikab@greeny.cs.tlu.ee>
2016-03-10 11:09:04 +0200

    Delete

--- table.php
+++ table.php
@@ -8,6 +8,34 @@
 	//create connection to database
 	
 	$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_dmikab");
+
+	/* if there is a ?DELETE=ROW_ID in the url
+	*/
+
+	if(isset($_GET["delete"])) {
+
+
+		echo "Deleting row with id:".$_GET["delete"];
+
+		$stmt = $mysql->prepare("Update messages_sample SET deleted=NOW() Where id = ?");
+
+		echo $mysql->error;
+
+		//replace the ?
+		$stmt->bind_param("i", $_GET["delete"]);
+		
+		if($stmt->execute()) {
+
+			echo "deleted successfully";
+		}else{
+
+			echo $stmt->errror;
+		}
+
+		//close the statement, so others can use connection
+
+		$stmt->close();
+	}
 	
 	//SQL sentence
 	
@@ -28,6 +56,7 @@ $table_html .="<tr>";
 $table_html .="<th>Recipient</th>";
 $table_html .="<th>Message</th>";
 $table_html .="<th>Created</th>";
+$table_html .="<th>Delete ?</th>";
 $table_html .="</tr>";
 
 
@@ -42,6 +71,7 @@ $table_html .="</tr>";
 		$table_html .= "<td>".$recipient."</td>";
 		$table_html .= "<td>".$message."</td>";
 		$table_html .= "<td>".$created."</td>";
+		$table_html .= "<td><a href = '?delete=".$id."'>x</a></td>";
 		$table_html .="</tr>"; //end row
 		
 		//DO something for EACH ROW
